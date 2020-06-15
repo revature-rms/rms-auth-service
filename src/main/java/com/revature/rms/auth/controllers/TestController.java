1ce3218ca52805a171663d5564ad06dbd0558c70
Wagenheim <wagenheimk@me.com>
2020-06-15 16:44:56 -0400

    added a test controller

---
+++ src/main/java/com/revature/rms/auth/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.revature.rms.auth.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String testMethod(){
+
+        return "application works";
+
+    }
+
+}
