43a421d55a4c03696df856abf5f2377a00752eab
Wagenheim <wagenheimk@me.com>
2020-06-16 13:23:07 -0400

    implemented an error handler

---
+++ src/main/java/com/revature/rms/auth/util/ControllerAdvisor.java
@@ -0,0 +1,36 @@
+package com.revature.rms.auth.util;
+
+import com.revature.rms.auth.dtos.ErrorResponse;
+import com.revature.rms.auth.exceptions.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RestControllerAdvice
+public class ControllerAdvisor {
+
+    @ExceptionHandler
+    public ErrorResponse handleException(RuntimeException e, HttpServletResponse resp){
+        ErrorResponse err = new ErrorResponse(e);
+
+        if(e instanceof AuthenticationException){
+            resp.setStatus(401);
+        } else if (e instanceof AuthorizationException){
+            resp.setStatus(403);
+        } else if(e instanceof BadRequestException){
+            resp.setStatus(400);
+        }else if (e instanceof InternalServerException){
+            resp.setStatus(500);
+        }else if (e instanceof ResourceNotFoundException){
+            resp.setStatus(404);
+        }else if (e instanceof ResourcePersistenceException){
+            resp.setStatus(409);
+        }
+
+        return err;
+    }
+
+}
