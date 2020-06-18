043027d170f2e57dce2b280d6cd4ef2dea778748
Wagenheim <wagenheimk@me.com>
2020-06-18 12:35:11 -0400

    added more dummy data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
-INSERT INTO APP_USER (EMAIL, PASSWORD, ROLE, USERNAME) VALUES ('wezley@revature.com', 'password', 'ADMIN', 'wsingleton')
-INSERT INTO APP_USER (EMAIL, PASSWORD, ROLE, USERNAME) VALUES ('kevin@revature.com', 'password', 'BASIC_USER', 'kwagenheim')
+INSERT INTO APP_USER (EMAIL, PASSWORD, ROLE, USERNAME) VALUES ('wezley@revature.com', 'password', 'Training Site Manager', 'wsingleton')
+INSERT INTO APP_USER (EMAIL, PASSWORD, ROLE, USERNAME) VALUES ('kevin@revature.com', 'password', 'Trainer', 'kwagenheim')
+INSERT INTO APP_USER (EMAIL, PASSWORD, ROLE, USERNAME) VALUES ('josef@revature.com', 'password', 'Building Manager', 'jbostik')
+INSERT INTO APP_USER (EMAIL, PASSWORD, ROLE, USERNAME) VALUES ('thomas@revature.com', 'password', 'Admin', 'thoang')
