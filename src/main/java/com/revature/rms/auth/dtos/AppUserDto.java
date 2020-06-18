66a971a1c09ad9b8cf3261e12a065c83ec305fc9
Wagenheim <wagenheimk@me.com>
2020-06-18 14:45:54 -0400

    implemented a dto for appuser

---
+++ src/main/java/com/revature/rms/auth/dtos/AppUserDto.java
@@ -0,0 +1,106 @@
+package com.revature.rms.auth.dtos;
+
+
+import com.revature.rms.auth.entities.AppUser;
+
+import java.util.Objects;
+
+public class AppUserDto {
+
+    private int id;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    public AppUserDto() {
+    }
+
+    public AppUserDto(int id, String email, String username, String password, String role) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public AppUserDto(AppUser user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.role = user.getRole().toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUserDto that = (AppUserDto) o;
+        return id == that.id &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(username, that.username) &&
+                Objects.equals(password, that.password) &&
+                Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, username, password, role);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUserDto{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
