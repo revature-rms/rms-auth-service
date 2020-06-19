faf9803ca042f13dbc9401bba7b4815c595484b5
Drew Peters <peters.drew.dp@gmail.com>
2020-06-19 13:10:12 -0400

    added method documentation for all repositories.

--- src/main/java/com/revature/rms/auth/repositories/AppUserRepository.java
+++ src/main/java/com/revature/rms/auth/repositories/AppUserRepository.java
@@ -7,7 +7,21 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
 
+    /**
+     * findAppUserById method: The id parameter is passed as the input.
+     * A user is returned when the input id matches a database record.
+     * @param id user id int
+     * @return user with matching id int
+     */
     AppUser findAppUserById(int id);
+
+    /**
+     * findAppUserByUsernameAndPassword: The username and password parameters are passed as the input.
+     * An appUser is returned when the input username and password Strings match database records.
+     * @param username appUser username cred
+     * @param password appUser password cred
+     * @return appUser with matching username and password Strings
+     */
     AppUser findAppUserByUsernameAndPassword(String username, String password);
 
 }
