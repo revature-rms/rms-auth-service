ba076aae1fb61af4b6739ad12cf52bb257e077f7
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-10-21 12:53:22 -0400

    Implemented post bean processing, tests still pass

--- src/main/java/com/revature/rms/auth/AuthServiceApplication.java
+++ src/main/java/com/revature/rms/auth/AuthServiceApplication.java
@@ -1,8 +1,10 @@
 package com.revature.rms.auth;
 
+import com.revature.rms.core.config.EurekaInstanceConfigBeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+    @Bean
+    public EurekaInstanceConfigBeanPostProcessor eurekaInstanceConfigBeanPostProcessor(){
+        return new EurekaInstanceConfigBeanPostProcessor();
     }
 
 }
